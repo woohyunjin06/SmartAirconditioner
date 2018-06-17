0028023cd5c6115f862aa11e7049531f37d9eada
woohyunjin06 <woohyunjin06@naver.com>
2018-06-17 23:35:41 +0900

    Add bluetooth send part.

--- app/src/main/java/com/smart/airconditioner/MainActivity.java
+++ app/src/main/java/com/smart/airconditioner/MainActivity.java
@@ -6,7 +6,12 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,12 +20,13 @@ import com.smart.airconditioner.network.BluetoothClient;
 import com.smart.airconditioner.network.DustInfo;
 import com.smart.airconditioner.network.WeatherInfo;
 
+import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     BluetoothClient client;
     DustInfo dInfo;
@@ -29,7 +35,13 @@ public class MainActivity extends AppCompatActivity {
     TextView mWeather;
     TextView mDust;
     TextView mTemp;
-    ProgressDialog pd;
+
+    Button auto;
+    Button custom;
+    SeekBar power;
+    Button timer;
+    Switch onoff;
+
 
 
     @Override
@@ -41,14 +53,63 @@ public class MainActivity extends AppCompatActivity {
     }
     public void init() {
         mWeather = findViewById(R.id.weather);
-        mDust =  findViewById(R.id.dust);
+        mDust = findViewById(R.id.dust);
         mTemp = findViewById(R.id.temperature);
 
+        auto = findViewById(R.id.auto);
+        custom = findViewById(R.id.custom);
+        power = findViewById(R.id.power);
+        timer = findViewById(R.id.timer);
+
+        onoff = findViewById(R.id.onoff);
+
+        initInterface();
         initDustInfo();
         initWeatherInfo();
         initClient();
     }
 
+    public void initInterface() {
+        auto.setOnClickListener(this);
+        custom.setOnClickListener(this);
+        timer.setOnClickListener(this);
+        onoff.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                int isCheck = isChecked ? 1 : 0;
+                try {
+                    handler.write(("P" + isCheck +"#").getBytes("UTF-8"));
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                auto.setEnabled(isChecked);
+                custom.setEnabled(isChecked);
+                power.setEnabled(isChecked);
+                timer.setEnabled(isChecked);
+            }
+
+        });
+        power.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                try {
+                    handler.write(("P" + (progress+1) +"#").getBytes("UTF-8"));
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
     /**
      * refresh :
      * dInfo.getCurrentDust();
@@ -85,21 +146,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-    public void showProgress(String msg) {
-        if( pd == null ) {
-            pd = new ProgressDialog(this);
-            pd.setCancelable(false);
-        }
 
-        pd.setMessage(msg);
-        pd.show();
-    }
-
-    public void hideProgress(){
-        if( pd != null && pd.isShowing() ) {
-            pd.dismiss();
-        }
-    }
     public void enableBluetooth() {
         client.enableBluetooth(this, new BluetoothClient.OnBluetoothEnabledListener() {
             @Override
@@ -124,8 +171,11 @@ public class MainActivity extends AppCompatActivity {
 
         deviceList.addAll(pairedDevices); // 근처 디바이스를 스캔한다.
         client.scanDevices(this, new BluetoothClient.OnScanListener() {
+            ProgressDialog pd = new ProgressDialog(MainActivity.this);
             @Override public void onStart() {//스캔 시작.
-
+                pd.setMessage("블루투스 기기를 찾는중... ");
+                pd.setCancelable(false);
+                pd.show();
             } @Override public void onFoundDevice(BluetoothDevice bluetoothDevice) { // 스캔이 완료된 디바이스를 받아온다.
                 if(deviceList.contains(bluetoothDevice)) {
                     deviceList.remove(bluetoothDevice);
@@ -135,6 +185,7 @@ public class MainActivity extends AppCompatActivity {
                 for(BluetoothDevice bd : deviceList) {
                     deviceListString.add(bd.getName());
                 }
+                pd.dismiss();
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                 builder.setTitle("연결할 블루투스 기기를 선택해주세요.");
                 builder.setItems(deviceListString.toArray(new CharSequence[deviceListString.size()]), new DialogInterface.OnClickListener() {
@@ -154,35 +205,7 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "블루투스를 사용할 수 없습니다", Toast.LENGTH_SHORT).show();
         }
     }
-    public void findDevice(){
-        final List<String> deviceListString = new ArrayList<>();
-        client.scanDevices(MainActivity.this, new BluetoothClient.OnScanListener() {
-            final ArrayList<BluetoothDevice> deviceList_2 = new ArrayList<>();
 
-            @Override
-            public void onStart() { // 스캔 시작.
-                showProgress("블루투스 기기를 찾는 중입니다");
-            }
-            @Override public void onFoundDevice(BluetoothDevice bluetoothDevice) { // 스캔이 완료된 디바이스를 받아온다.
-                Log.d("bt event", "FOUND");
-                deviceList_2.add(bluetoothDevice);
-            }
-            @Override public void onFinish() { // 스캔 종료.
-                for(BluetoothDevice bd : deviceList_2) {
-                    deviceListString.add(bd.getName());
-                }
-                hideProgress();
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle("연결할 블루투스 기기를 선택해주세요.");
-                builder.setItems(deviceListString.toArray(new CharSequence[deviceListString.size()]), new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        connectDevice(deviceList_2.get(which));
-                    }
-                }).show();
-            }
-        });
-    }
     @Override
     protected void onDestroy() {
         if(client!=null)
@@ -204,7 +227,7 @@ public class MainActivity extends AppCompatActivity {
         public void onDisconnected() {
             // 연결 종료 이벤트.
         }
-        ByteBuffer mmByteBuffer = ByteBuffer.allocate(1024);
+        ByteBuffer mmByteBuffer = ByteBuffer.allocate(50);
 
         @Override
         public void onData(byte[] buffer, int length) {
@@ -215,10 +238,78 @@ public class MainActivity extends AppCompatActivity {
                 mmByteBuffer = newBuffer;
             }
             mmByteBuffer.put(buffer, 0, length);
-            if(buffer[length - 1] == '\0' && length != 1) {
+            String data = new String(mmByteBuffer.array());
+            if(data.trim().length()<0)
+                return;
+            if(buffer[length - 1] == '>' && length != 1) {
                 Toast.makeText(MainActivity.this, new String(mmByteBuffer.array(), 0, mmByteBuffer.position()), Toast.LENGTH_SHORT).show();
                 mmByteBuffer.clear();
             }
         }
     };
+
+    @Override
+    public void onClick(View v) {
+        String message;
+        try {
+            switch (v.getId()) {
+                case R.id.auto:
+                    message = "M0#";
+                    handler.write(message.getBytes("UTF-8"));
+                    break;
+
+                case R.id.custom:
+                    message = "M1#";
+                    handler.write(message.getBytes("UTF-8"));
+                    break;
+
+                case R.id.timer:
+                    showEditDialog();
+                    break;
+            }
+        }catch(UnsupportedEncodingException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void showEditDialog(){
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        alert.setTitle("전원을 몇 시간 뒤에 끄시겠습니까? (1시간 단위)");
+
+        final EditText name = new EditText(this);
+        alert.setView(name);
+
+        alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                String time = name.getText().toString();
+                int time_int = 0;
+                try {
+                    time_int = Integer.parseInt(time);
+                } catch(NumberFormatException e){
+                    e.printStackTrace();
+                    Toast.makeText(MainActivity.this, "올바른 숫자가 아닙니다.", Toast.LENGTH_SHORT).show();
+                }
+
+                if(time_int>20||time_int<1){
+                    Toast.makeText(MainActivity.this, "1이상, 20이하의 숫자를 입력해주세요.", Toast.LENGTH_SHORT).show();
+                }
+                try {
+                    handler.write(("T"+time+"#").getBytes("UTF-8"));
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+        alert.setNegativeButton("취소",new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                dialog.dismiss();
+            }
+        });
+
+        alert.show();
+
+    }
 }
